f2c207c39021a118f4bd00586e3c6b4af77156a2
katlego <katlegokgwedi6@gmail.com>
2025-01-29 22:26:09 +0200

    another test

--- hot-spot-web-app/src/components/HomePage.js
+++ hot-spot-web-app/src/components/HomePage.js
@@ -3,10 +3,10 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1>Welcome to Hot-Spot SAMSUNG Service Centre</h1>
       <p>This is the home page of the application.</p>
     </div>
   );
-};
+}; 
 
 export default HomePage;
